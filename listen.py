f8a4bd98cc853db4eb253d3bde519cd332d819ed
SteveWooo <461437874@qq.com>
2018-08-21 00:07:30 +0800

    init

---
+++ listen.py
@@ -0,0 +1,27 @@
+import sys
+import time as time
+from scapy.all import (
+	get_if_hwaddr,
+	getmacbyip,
+	ARP,
+	Ether,
+	sendp,
+	sniff,
+	wrpcap,
+	Raw,
+	TCP,
+	IP
+)
+import scapy_http.http as HTTP
+
+def callback(pkt):
+	if HTTP.HTTPRequest in pkt:
+		payload = pkt[TCP].payload
+		if payload.Method == "GET":
+			url = "http://{0}{1}".format(payload.Host, payload.Path)
+			if url.find(".jpg") > 0:
+				print url
+				print payload.Headers
+				print "====="
+
+result = sniff(filter="tcp and port 80", prn=callback, iface="en0")
